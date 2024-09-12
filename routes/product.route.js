d95e8a354e42ce14ccafc4a375796313270a6b5d
waqar-nawaz <waqar@replatechnologies.com>
2024-09-12 22:59:01 +0500

    test

--- routes/product.route.js
+++ routes/product.route.js
@@ -7,9 +7,6 @@ const productsSchema = require('../validation/products.schema');
 const validateRequest = require('../middleware/validateRequest');
 
 
-
-
-
 route.post("/product", isAuth, validateRequest(productsSchema), prodcutController.addProduct);
 route.get("/product", isAuth, prodcutController.getProduct);
 
